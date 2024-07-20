daa02df38166dc16b5a3984c6fe3a165c9c0704c
serhat koç <kocserhat671@gmail.com>
2024-07-20 14:23:28 +0300

    başlangıç

---
+++ app/components/pomodoroWrapper.js
@@ -0,0 +1,4 @@
+export function PomodoroWrapper() {
+  return (
+    <div>pomodoroWrapper</div>
+    )};
---
+++ store/provider.js
@@ -0,0 +1,11 @@
+"use client";
+
+import {createTheme, ThemeProvider} from "@mui/material"
+
+export const Provider = ({children}) => {
+return (
+    <ThemeProvider theme={createTheme()}>
+        {children}
+    </ThemeProvider>
+)
+}
